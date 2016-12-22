19a693243ce4e3b59178db9761443752dece5495
liuyongfei <liuyongfei@yunzongnet.com>
2016-12-22 23:54:04 +0800

    修改router/index.js里的路由写法 没有找到error flash不显示的原因

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var settings = require('./setting')
-// var MongoStore = require('connect-mongo')
 var session = require('express-session')
 var MongoStore = require('connect-mongo')(session)
 var methodOverride = require('method-override');
 var flash = require('connect-flash')
+
 var router = express.Router();
+var routes = require('./routes/index')
 
 var app = express();
 
+// console.log(routess.test)
 // app.configure(function(){
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
@@ -48,7 +50,7 @@ var app = express();
   app.use(flash());
 
   // app.use(app.router)
-  app.use(router)
+  // app.use(router)
   app.use(express.static(__dirname + '/public'))
 // })
 
@@ -63,8 +65,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+
+app.get('/',routes.index)
+app.get('/reg',routes.reg)
+app.post('/reg',routes.doReg)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -82,6 +88,9 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  // res.locals.errors = req.flash('error');
+  // res.locals.infos = req.flash('info');
+  // next();
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,24 @@ var crypto = require('crypto')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/reg',function (req, res, next) {
-  res.render('reg',{title: '用户注册'})
-})
-router.post('/reg',function (req, res, next) {
-  // 检查秘密是否一致
-  if (req.body['password-repeat'] != req.body['password']) {
-    req.flash('error','两次输入的密码不一致')
-    res.redirect('/reg')
-  } else {
-    res.redirect('/')
-  }
-})
-module.exports = router;
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// exports.test = function () {
+//   console.log('world')
+// }
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express' });
+}
+exports.reg = function (req, res) {
+    res.render('reg', { title: '用户注册' });
+}
+exports.doReg = function(req, res) {
+	//检查密码
+    if (req.body['password-repeat'] != req.body['password']) {
+		req.flash('error', '两次输入的密码不一致')
+		return res.redirect('/reg')
+    }
+}
+
+// module.exports = router
