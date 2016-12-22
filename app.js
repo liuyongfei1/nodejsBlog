548f1e8ee8109566816e5b72b2101b6dd586aad0
liuyongfei <liuyongfei@yunzongnet.com>
2016-12-22 22:20:50 +0800

    添加注册页面
    修改app.js里的一些新组建的写法；
    添加注册页面

--- app.js
+++ app.js
@@ -9,31 +9,48 @@ var partials = require('express-partials');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var settings = require('./settings')
-var MongoStore = require('connect-mongo')
+var settings = require('./setting')
+// var MongoStore = require('connect-mongo')
+var session = require('express-session')
+var MongoStore = require('connect-mongo')(session)
+var methodOverride = require('method-override');
+var flash = require('connect-flash')
+var router = express.Router();
 
 var app = express();
 
-app.configure(function(){
+// app.configure(function(){
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser()); // cookie解析中间件
+
+  app.set('trust proxy', 1) // trust first proxy
+  // app.use(express.bodyParser());
+  app.use(require('body-parser').urlencoded({extended: true}))
+  // app.use(express.methodOverride());
+  app.use(methodOverride())
+  // app.use(express.cookieParser()); // cookie解析中间件
+  app.use(cookieParser())
   // 提供会话支持，设置它的store参数为MongoStore实例，把会话信息存储到数据库中去，以避免数据丢失
-  app.use(express.session({
+  app.use(session({
     secret : settings.cookieSecret,
     cookie : {
       maxAge : 60000 * 20	//20 minutes
     },
     store : new MongoStore({
-      db : settings.db
-    })
+      db : settings.db,
+      url : 'mongodb://localhost/microblog'
+    }),
+    resave : false,
+    saveUninitialized : false,
   }))
-  app.use(app.router)
+
+  app.use(flash());
+
+  // app.use(app.router)
+  app.use(router)
   app.use(express.static(__dirname + '/public'))
-})
+// })
 
 
 // 启用layout
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var crypto = require('crypto')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/reg',function (req, res, next) {
+  res.render('reg',{title: '用户注册'})
+})
+router.post('/reg',function (req, res, next) {
+  // 检查秘密是否一致
+  if (req.body['password-repeat'] != req.body['password']) {
+    req.flash('error','两次输入的密码不一致')
+    res.redirect('/reg')
+  } else {
+    res.redirect('/')
+  }
+})
 module.exports = router;
