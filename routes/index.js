4b95449b226d2a26f550c9c71b83a700b610185d
liuyongfei <liuyongfei198805@gmail.com>
2016-12-23 23:47:12 +0800

    给密码加密
    给输入的密码使用crypto库的md5进行加密

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,16 @@ exports.reg = function (req, res) {
     res.render('reg', { title: '用户注册' });
 }
 exports.doReg = function(req, res) {
-	//检查密码
+  //检查密码
     if (req.body['password-repeat'] != req.body['password']) {
 		req.flash('error', '两次输入的密码不一致')
 		return res.redirect('/reg')
     }
+
+    //生成md5的密码
+    var md5 = crypto.createHash('md5')
+    var password = md5.update(req.body.password).digest('base64')
+  
 }
 
 // module.exports = router
