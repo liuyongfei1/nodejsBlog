b642b6776080f8eaec2128a90122bde4ba8eee63
liuyongfei <liuyongfei198805@gmail.com>
2017-01-05 23:01:26 +0800

    修改routes/index.js，使用express.router()

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ var express = require('express');
 var crypto = require('crypto')
 var User = require('../models/user.js')
 var Post = require('../models/post.js')
-// var router = express.Router()
-// var url = require('url')
+var router = express.Router()
 
 /* GET home page. */
-exports.index = function (req, res) {
+router.get('/',function (req, res) {
   Post.get(null,function (err,posts) {
     if (err) {
       posts = []
@@ -21,18 +20,41 @@ exports.index = function (req, res) {
     })
   })
 
-}
+})
+// exports.index = function (req, res) {
+//   Post.get(null,function (err,posts) {
+//     if (err) {
+//       posts = []
+//     }
+//     res.render('index',
+//     {
+//       title: '首页',
+//       posts : posts,
+// 			user : req.session.user,
+// 			success : req.flash('success').toString(),
+// 			error : req.flash('error').toString()
+//     })
+//   })
+// }
 
 // 用户注册
-exports.reg = function (req, res) {
+router.get('/reg',function (req, res) {
     res.render('reg', {
       title: '用户注册',
       user : req.session.user,
       success : req.flash('success').toString(),
       error : req.flash('error').toString()
     });
-}
-exports.doReg = function(req, res) {
+})
+// exports.reg = function (req, res) {
+//     res.render('reg', {
+//       title: '用户注册',
+//       user : req.session.user,
+//       success : req.flash('success').toString(),
+//       error : req.flash('error').toString()
+//     });
+// }
+router.post('/reg',function(req, res) {
   //检查密码
     if (req.body['password-repeat'] != req.body['password']) {
   		req.flash('error', '两次输入的密码不一致')
@@ -69,9 +91,47 @@ exports.doReg = function(req, res) {
       })
     })
 
-}
+})
+// exports.doReg = function(req, res) {
+//   //检查密码
+//     if (req.body['password-repeat'] != req.body['password']) {
+//   		req.flash('error', '两次输入的密码不一致')
+//   		return res.redirect('/reg')
+//     }
+//
+//     //生成md5的密码
+//     var md5 = crypto.createHash('md5')
+//     var password = md5.update(req.body.password).digest('base64')
+//
+//     var newUser = new User({
+//       name : req.body.username,
+//       password : password
+//     })
+//
+//     // 检查用户名是否已经存在
+//     User.get(newUser.name,function (err,user) {
+//       if (user)
+//         err = 'Username already exists'
+//       if (err) {
+//         req.flash('error',err)
+//         return res.redirect('/reg')
+//       }
+//
+//       // 如果不存在则新增用户
+//       newUser.save(function (err) {
+//         if (err) {
+//           req.flash('error',err)
+//           return res.redirect('/reg')
+//         }
+//         req.session.user = newUser
+//         req.flash('success','注册成功')
+//         res.redirect('/')
+//       })
+//     })
+//
+// }
 // 用户登录
-exports.login = function (req,res) {
+router.get('/login',function (req,res) {
   res.render('login',{
     title : '用户登录',
     user : req.session.user,
@@ -79,8 +139,16 @@ exports.login = function (req,res) {
     error : req.flash('error').toString()
   })
 }
-
-exports.doLogin = function (req,res) {
+)
+// exports.login = function (req,res) {
+//   res.render('login',{
+//     title : '用户登录',
+//     user : req.session.user,
+//     success : req.flash('success').toString(),
+//     error : req.flash('error').toString()
+//   })
+// }
+router.post('/login',function (req,res) {
   // 生成散列值
   var md5 = crypto.createHash('md5')
   var password = md5.update(req.body.password).digest('base64')
@@ -100,9 +168,30 @@ exports.doLogin = function (req,res) {
 		req.flash('success', '登录成功');
 		res.redirect('/');
   })
-}
+})
+// exports.doLogin = function (req,res) {
+//   // 生成散列值
+//   var md5 = crypto.createHash('md5')
+//   var password = md5.update(req.body.password).digest('base64')
+//
+//   User.get(req.body.username,function (err,user) {
+//
+//     if (!user) {
+// 			req.flash('error', '用户不存在')
+// 			return res.redirect('/login')
+// 		}
+//     if (user.password != password) {
+// 			req.flash('error', '密码错误')
+//       return res.redirect('/login')
+// 		}
+//
+//     req.session.user = user;
+// 		req.flash('success', '登录成功');
+// 		res.redirect('/');
+//   })
+// }
 // 用户发布微博
-exports.publish = function (req,res) {
+router.post('/publish',function (req,res) {
   var currentUser = req.session.user
   var post = new Post(currentUser.name,req.body.post)
   post.save(function (err) {
@@ -113,9 +202,21 @@ exports.publish = function (req,res) {
     req.flash('success','发表成功')
     res.redirect('/u/' + currentUser.name)
   })
-}
+})
+// exports.publish = function (req,res) {
+//   var currentUser = req.session.user
+//   var post = new Post(currentUser.name,req.body.post)
+//   post.save(function (err) {
+//     if (err) {
+//       req.flash('error',err)
+//       return res.redirect('/')
+//     }
+//     req.flash('success','发表成功')
+//     res.redirect('/u/' + currentUser.name)
+//   })
+// }
 // 展示用户发布的微博
-exports.show = function (req,res) {
+router.get('show',function (req,res) {
   User.get(req.params.user,function (err,user) {
     if (!user) {
       req.flash('error','用户不存在')
@@ -136,13 +237,40 @@ exports.show = function (req,res) {
       })
     })
   })
-}
+})
+// exports.show = function (req,res) {
+//   User.get(req.params.user,function (err,user) {
+//     if (!user) {
+//       req.flash('error','用户不存在')
+//       return redirect('/')
+//     }
+//     Post.get(user.name,function (err,posts) {
+//       if (err) {
+//         req.flash('error',err)
+//         return res.redirect('/')
+//       }
+//       // console.dir(user, {colors: true})
+//       res.render('user',{
+//         title : user.name,
+//         posts: posts,
+// 				user : req.session.user,
+// 				success : req.flash('success').toString(),
+// 				error : req.flash('error').toString()
+//       })
+//     })
+//   })
+// }
 // 退出动作
-exports.logout =  function(req,res) {
+router.get('/login',function(req,res) {
   req.session.user = null;
   req.flash('success','退出成功')
   res.redirect('/')
-}
+})
+// exports.logout =  function(req,res) {
+//   req.session.user = null;
+//   req.flash('success','退出成功')
+//   res.redirect('/')
+// }
 
 // 检查是否没有登录
 exports.checkNotLogin = function (req,res,next) {
@@ -162,4 +290,4 @@ exports.checkLogin = function (req,res,next) {
   next()
 }
 
-// module.exports = router
+module.exports = router
