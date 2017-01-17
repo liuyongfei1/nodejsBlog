bb4542af70cd117cbd74a2c207eeae23ea459642
liuyongfei <liuyongfei198805@gmail.com>
2017-01-17 18:49:26 +0800

    添加404页面，暂时把该部分代码屏蔽了

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ module.exports = function (app) {
   app.use('/login',require('./login'))
   app.use('/logout',require('./logout'))
   app.use('/posts',require('./posts'))
+  // 404 page 可以暂时不用
+  // app.use(function (req, res) {
+  //   if (!res.headersSent) {
+  //     res.render('404',{
+  //       title : 'Not Found',
+  //     });
+  //   }
+  // });
 }
