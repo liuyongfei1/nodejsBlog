0e12a138a876390ff9b7ed74e13b9bec67836bec
liuyongfei <liuyongfei198805@gmail.com>
2017-01-17 10:05:56 +0800

    完成用户发表微博页面和查看详细的文章页

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,13 @@ router.get('/',function (req,res,next) {
     .catch(next)
 })
 
+// GET /posts/create 发表文章页
+router.get('/create',checkLogin,function (req,res,next) {
+  res.render('create',{
+    'title' : '发表文章'
+  })
+})
+
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId',function (req,res,next) {
   var postId = req.params.postId
@@ -36,13 +43,7 @@ router.get('/:postId',function (req,res,next) {
   .catch(next);
 })
 
-// GET /posts/create 发表文章页
-router.get('/create',checkLogin,function (req,res,next) {
-  res.render('create',{
-    'title' : '发表文章'
-  })
-})
-
+// POST /posts 发表一篇文章
 router.post('/',checkLogin,function (req,res,next) {
   var author = req.session.user._id
   var title = req.body.title
