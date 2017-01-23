8fc10baffc65186ccb592d3aea46b19471b6a1db
liuyongfei <liuyongfei198805@gmail.com>
2017-01-23 18:36:59 +0800

    编写用户注册的测试用例

--- test/signup.js
+++ test/signup.js
@@ -1,8 +1,39 @@
+var path = require('path');
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var request = require('supertest');
+var app = require('../index');
+var User = require('../lib/mongo').User;
+
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal(-1, [1,2,3].indexOf(4));
+//     });
+//   });
+// });
+
+var testName1 = 'testName1';
+var testName2 = 'nswbmw';
+
+describe('reg',function () {
+  describe('POST /reg',function () {
+    var agent = request.agent(app);//persist cookie when redirect
+    beforeEach(function (done) {
+      // 创建一个用户
+      User.create({
+        name: testName1,
+        password: '123456',
+        avatar: '',
+        gender: 'x',
+        bio: ''
+      })
+      .exec()
+      .then(function () {
+        done();
+      })
+      .catch(done);
+    });
+
     });
   });
 });
